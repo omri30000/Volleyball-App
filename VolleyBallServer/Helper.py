2f7e9f85e2a1e5b6822e4b517701a5d9b1bc50ac
Omri Zaiman <omri162004@gmail.com>
2020-02-24 15:26:24 +0200

    added helper file (read and write to database)

---
+++ VolleyBallServer/Helper.py
@@ -0,0 +1,23 @@
+import json
+
+players = {1:1, 2:2, 3:3}
+
+def write_dict_to_file(players, file_name):
+    '''
+    the function will get the dict of players and write it into a file
+    input: dict of players and name of file
+    output: none
+    '''
+    with open(file_name,'w') as file:
+        file.write(str(players))
+
+
+def read_file_to_dict(file_name):
+    '''
+    the function will get the file name and read a dict of players from inside it
+    input: dict of players and name of file
+    output: dict of players
+    '''
+    with open(file_name, 'r') as file:
+        players = eval(file.read()) 
+    return players
--- VolleyBallServer/Server.py
+++ VolleyBallServer/Server.py
@@ -2,12 +2,16 @@ import socket
 import re
 import collections
 import _thread
+import Helper
 
 LISTEN_PORT = 2019
 
-
+data_base = {}
 
 def build_server():
+    # parse data base file to internal dict data base
+    data_base = Helper.read_file_to_dict("players.txt")
+
     # Create a TCP/IP socket
     listening_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Connecting to remote computer with port 2019
@@ -37,9 +41,6 @@ def manage_server(listening_sock):
             print("Error: [WinError 10054] An existing connection was forcibly closed by the remote host")
     listening_sock.close()  # isn't necessary because the server will be closed manually
 
+
 def manage_conversation():
-    pass
-
-
-def add_user_to_system():
     pass
