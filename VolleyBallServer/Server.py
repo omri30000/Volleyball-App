f65f74537e77afd4159e607e9f19a45676c25382
Omri Zaiman <omri162004@gmail.com>
2020-02-24 16:23:18 +0200

    updated server

--- VolleyBallServer/Server.py
+++ VolleyBallServer/Server.py
@@ -5,13 +5,16 @@ import _thread
 import Helper
 
 LISTEN_PORT = 2019
+VALID_PLAYERS_FILE_NAME = "Players.txt"
+ATTENDING_EVENT_FILE_NAME = "20_2_2020.txt"
 
-data_base = {}
+def main():
+    pass
 
 def build_server():
     # parse data base file to internal dict data base
     data_base = Helper.read_file_to_dict("players.txt")
-
+    
     # Create a TCP/IP socket
     listening_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Connecting to remote computer with port 2019
@@ -24,7 +27,6 @@ def manage_server(listening_sock):
     # start listening
     listening_sock.listen(1)
     
-    data_base = ""
     count_users = 0
     while True:
         # new conversation socket
@@ -32,7 +34,7 @@ def manage_server(listening_sock):
         # from now on, the client and server are connected
 
         try:
-            _thread.start_new_thread(manage_conversation, (client_soc, data_base))
+            _thread.start_new_thread(manage_conversation, client_soc)
             count_users += 1
 
 
@@ -42,5 +44,36 @@ def manage_server(listening_sock):
     listening_sock.close()  # isn't necessary because the server will be closed manually
 
 
-def manage_conversation():
+def manage_conversation(client_soc):
+    client_msg = client_soc.recv(2048).decode()
+
+    player_name = client_msg[client_msg.find("#") + 1:]
+    player_name = player_name[0:player_name.find("_")] + ' '  + player_name[player_name.find("_") + 1:]
+    player_name = player_name[0: player_name.find("$")]
+
+    print(player_name)
+
+    if check_if_user_known(player_name, VALID_PLAYERS_FILE_NAME):
+        pass
+
+
+    #client_soc.sendall("your input is invalid, try again!".encode())
     pass
+
+def check_if_user_known(user_name, database_file):
+    """
+    the function will check if a player is recognized (appears in players file)
+    input: the name of the player and the file of valid players
+    output: boolean if player is valid or not
+    """
+    valid_players = Helper.read_file_to_dict(database_file) # dict of players
+
+    if user_name in valid_players.keys():
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    main()
+    
