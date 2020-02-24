32849d9351b31b4492b017a7fb655838ab850c9a
Ofir Shapira <ofirs2003@gmail.com>
2020-02-24 17:58:45 +0200

    improve divding

--- VolleyBallServer/build_teams.py
+++ VolleyBallServer/build_teams.py
@@ -1,5 +1,5 @@
 import Helper
-
+#shufle
 def divide_teams(players): #dict of { players : level }
     team_one ={}
     team_two = {}
@@ -25,40 +25,67 @@ def from_dict_to_sorted_list(players):
     parts =Helper.from_dict_to_list(players)
     return sorted(parts,key = by_value,reverse= True)
 
+git
+
+def divide_teams_smart(players):
+    parts = from_dict_to_sorted_list(players)
+    team1,team2 = fill_teams(players,[],[])
 
 
-"""
-def add_next(players,team1,team2):
+
+def fill_teams(players,team1,team2):
+    
+    
     if len(players) == 0:
         return abs(get_team_level(team2) - get_team_level(team1))
-   
 
-    team1_copy = {}
-    team2_copy = {}
 
-    if num > num2:
+    if len(team1) > len(team2) + len(players) +1: # is it bigger than one
         raise "erorr mate!"
-    player = players.items()[0]
-    del players[player[0]]
-   
+
+    
+    cond1,cond2 = True,True
+
+    team1_copy = copy_list(team1)
+    team2_copy = copy_list(team2)
+    player = players[0]
+    del players[0]
     #option one -> team 1
-    team1[player[0]] =player[1] 
-    val1 = add_next(players,team1,team2)
+    #team
+    team1_copy += [player]
+    try:
+        val1,team1_copy1,team2_copy1 = fill_teams(players,team1_copy,team2_copy)
+    except:
+        cond1 = False        
+
+
     #option two -> team 2
-    del team1[player[0]]
+    del team1_copy[-1]
+    team2_copy += [player]
+    try:
+        val2, team1_copy2,team2_copy = fill_teams(players,team1_copy,team2_copy)
+
+        del team2_copy[-1]
+    except:
+        cond2 = False
+
+    if not cond and not cond2:
+        raise "error mate"
+
+
+
     team2[player[0]] = player[1] 
     del team1[player[0]]
 
 def get_team_level(team):
     sum = 0
-    for val in team.values():
-        sum += val
+    for val in team:
+        sum += val[i]
     return sum 
 
 
-def copy_dict(team):
-    team_copy = {}
-    for i in team.items():
-        team_copy[i[0]] = i[1]
+def copy_list(team):
+    team_copy = []
+    for i in team:
+        team_copy += [i]
     return team_copy
-    """
