2d49b10cb34bdb76c2a3d3be38264f3e7e694013
Ofir Shapira <ofirs2003@gmail.com>
2020-02-26 18:43:50 +0200

    added fourth activity to manifest

--- VolleyBallClientAndroid/app/src/main/java/com/example/volleyballclient/FourthActivity.java
+++ VolleyBallClientAndroid/app/src/main/java/com/example/volleyballclient/FourthActivity.java
@@ -12,7 +12,6 @@ public class FourthActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fourth);
 
     }
 }
