8356c2c1c843a2564ffcb862385454699dae0323
Omri Zaiman <omri162004@gmail.com>
2020-02-26 18:11:53 +0200

    updated second activity

--- VolleyBallClientAndroid/app/src/main/java/com/example/volleyballclient/SecondActivity.java
+++ VolleyBallClientAndroid/app/src/main/java/com/example/volleyballclient/SecondActivity.java
@@ -3,12 +3,35 @@ package com.example.volleyballclient;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button addMeBtn;
+    private Button addFriendBtn;
+    private int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        addMeBtn = (Button)findViewById(R.id.btnEnrMyself);
+        addFriendBtn = (Button)findViewById(R.id.btnEnrFriend);
+
+        addMeBtn.setOnClickListener(this);
+        addFriendBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == addMeBtn)
+        {
+            count++;
+        }
+        else if (v == addFriendBtn)
+        {
+            count++;
+        }
     }
 }
